41ec9a4f8d5e946a14fa824c20550bc3fe5de833
GrettaSmith <49281543+GrettaSmith@users.noreply.github.com>
2023-04-11 14:38:57 +0300

    Update UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,7 +6,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 import java.util.Set;
 
-public interface UserStorage extends Storage<User>{
+public interface UserStorage {
 
     List<User> getUsersList();
 
