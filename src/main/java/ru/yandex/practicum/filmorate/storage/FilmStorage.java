5e2e9bf4c85358e14de5f2dcc1f4067cfde6c486
GrettaSmith <49281543+GrettaSmith@users.noreply.github.com>
2023-04-11 14:30:47 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,7 +6,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface FilmStorage extends Storage <Film>  {
+public interface FilmStorage extends Storage  <Film>  {
 
     List<Film> getFilmsList() throws SQLException;
 
